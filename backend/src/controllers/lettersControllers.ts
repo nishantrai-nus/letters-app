238d8994a27a614b9af58484b32f1c53fe4ac3ab
nishantrai-nus <e0560182@u.nus.edu>
2023-07-21 13:31:33 +0800

    improve error handling

--- backend/src/controllers/lettersControllers.ts
+++ backend/src/controllers/lettersControllers.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import LetterModel from "../models/letter";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getLetters: RequestHandler = async(req, res, next) => {
     try {
@@ -14,17 +16,36 @@ export const getLetters: RequestHandler = async(req, res, next) => {
 export const getLetter: RequestHandler = async(req, res, next) => {
     const letterId = req.params.letterId;
     try {
+        if (!mongoose.isValidObjectId(letterId)) {
+            throw createHttpError(400, "Invalid letter id")
+        }
+        
         const letter = await LetterModel.findById(letterId).exec();
+
+        if (!letter) {
+            throw createHttpError(404, "Letter not found");
+        }
+
         res.status(200).json(letter)
     } catch (error) {
         next(error)
     }
 }
 
-export const createLetter: RequestHandler = async(req, res, next) => {
+interface CreateLetterBody {
+    title?: string, 
+    text?: string, 
+}
+
+export const createLetter: RequestHandler<unknown, unknown, CreateLetterBody, unknown> = async(req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
+    
     try {
+        if (!title) {
+            throw createHttpError(400, "Letter must have a title");
+        }
+        
         const newLetter = await LetterModel.create({
             title: title,
             text: text,
